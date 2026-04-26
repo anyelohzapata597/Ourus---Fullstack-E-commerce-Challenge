b6c1316acd0bd3610cf105c5b15ff9904313df9e
anyelo hoyos <anyelo.hoyos@upb.edu.co>
2026-04-25 20:25:10 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Ourus---Fullstack-E-commerce-Challenge/',
   plugins: [react(), tailwindcss()],
   
   // Resolver path aliases
